4fd07ceb33227d140240187f1f7919ba2c6c6b32
bewr <bw@shoutrlabs.com>
2020-07-22 00:29:29 +0200

    add persistent settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,10 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~/plugins/axiosGlobalInterceptors'],
+  plugins: [
+    '~/plugins/axiosGlobalInterceptors',
+    { src: '~/plugins/localStorage', ssr: false },
+  ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
